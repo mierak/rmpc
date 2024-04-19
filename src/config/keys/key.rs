f3fd702fae3df4ad2acfd04eeb19a2c8f033dd55
MieraK <mrkpwnz@gmail.com>
2024-04-20 01:14:08 +0200

    feat: better format for key config

---
+++ src/config/keys/key.rs
@@ -0,0 +1,248 @@
+use std::{fmt::Display, str::FromStr};
+
+use crossterm::event::{KeyCode, KeyModifiers};
+use itertools::Itertools;
+use serde_with::{DeserializeFromStr, SerializeDisplay};
+
+#[derive(Debug, SerializeDisplay, DeserializeFromStr, PartialEq, Eq, Hash)]
+pub struct Key {
+    pub key: KeyCode,
+    pub modifiers: KeyModifiers,
+}
+
+impl Display for Key {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let has_ctrl = self.modifiers.contains(KeyModifiers::CONTROL);
+        let has_alt = self.modifiers.contains(KeyModifiers::ALT);
+        let has_shift = self.modifiers.contains(KeyModifiers::SHIFT);
+        let has_no_modifiers = !has_ctrl && !has_alt && !has_shift;
+
+        if has_ctrl || has_alt || (has_shift && !matches!(self.key, KeyCode::Char(c) if c.is_alphabetic())) {
+            write!(f, "<")?;
+        }
+        if has_ctrl {
+            write!(f, "C-")?;
+        }
+        if has_alt {
+            write!(f, "A-")?;
+        }
+        if has_shift && !matches!(self.key, KeyCode::Char(c) if c.is_alphabetic()) {
+            write!(f, "S-")?;
+        }
+
+        match self.key {
+            KeyCode::Backspace if has_no_modifiers => write!(f, "<BS>"),
+            KeyCode::Backspace => write!(f, "BS"),
+            KeyCode::Enter if has_no_modifiers => write!(f, "<CR>"),
+            KeyCode::Enter => write!(f, "CR"),
+            KeyCode::Left if has_no_modifiers => write!(f, "<Left>"),
+            KeyCode::Left => write!(f, "Left"),
+            KeyCode::Right if has_no_modifiers => write!(f, "<Right>"),
+            KeyCode::Right => write!(f, "Right"),
+            KeyCode::Up if has_no_modifiers => write!(f, "<Up>"),
+            KeyCode::Up => write!(f, "Up"),
+            KeyCode::Down if has_no_modifiers => write!(f, "<Down>"),
+            KeyCode::Down => write!(f, "Down"),
+            KeyCode::Home if has_no_modifiers => write!(f, "<Home>"),
+            KeyCode::Home => write!(f, "Home"),
+            KeyCode::End if has_no_modifiers => write!(f, "<End>"),
+            KeyCode::End => write!(f, "End"),
+            KeyCode::PageUp if has_no_modifiers => write!(f, "<PageUp>"),
+            KeyCode::PageUp => write!(f, "PageUp"),
+            KeyCode::PageDown if has_no_modifiers => write!(f, "<PageDown>"),
+            KeyCode::PageDown => write!(f, "PageDown"),
+            KeyCode::Tab if has_no_modifiers => write!(f, "<Tab>"),
+            KeyCode::Tab => write!(f, "Tab"),
+            KeyCode::BackTab if has_no_modifiers => write!(f, "<Tab>"),
+            KeyCode::BackTab => write!(f, "Tab"),
+            KeyCode::Delete if has_no_modifiers => write!(f, "<Del>"),
+            KeyCode::Delete => write!(f, "Del"),
+            KeyCode::Insert if has_no_modifiers => write!(f, "<Insert>"),
+            KeyCode::Insert => write!(f, "Insert"),
+            KeyCode::Esc if has_no_modifiers => write!(f, "<Esc>"),
+            KeyCode::Esc => write!(f, "Esc"),
+            KeyCode::F(num) if has_no_modifiers => write!(f, "<F{num}>"),
+            KeyCode::F(num) => write!(f, "F{num}"),
+            KeyCode::Char(' ') if has_no_modifiers => write!(f, "<Space>"),
+            KeyCode::Char(' ') => write!(f, "Space"),
+            KeyCode::Char(char) => write!(f, "{char}"),
+            KeyCode::CapsLock
+            | KeyCode::ScrollLock
+            | KeyCode::NumLock
+            | KeyCode::PrintScreen
+            | KeyCode::Pause
+            | KeyCode::Menu
+            | KeyCode::KeypadBegin
+            | KeyCode::Media(_)
+            | KeyCode::Modifier(_)
+            | KeyCode::Null => Ok(()),
+        }?;
+
+        if has_ctrl || has_alt || (has_shift && !matches!(self.key, KeyCode::Char(c) if c.is_alphabetic())) {
+            write!(f, ">")?;
+        }
+
+        Ok(())
+    }
+}
+
+impl FromStr for Key {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let chars = s.chars().collect_vec();
+        let mut modifiers = KeyModifiers::NONE;
+        let mut i = 0;
+
+        let mut key_part_range = 0..chars.len();
+        loop {
+            let Some(c) = chars.get(i) else {
+                break;
+            };
+            let next = chars.get(i + 1);
+
+            match c {
+                'C' if next.is_some_and(|v| v == &'-') => {
+                    modifiers |= KeyModifiers::CONTROL;
+                    i += 1;
+                }
+                'A' if next.is_some_and(|v| v == &'-') => {
+                    modifiers |= KeyModifiers::ALT;
+                    i += 1;
+                }
+                'S' if next.is_some_and(|v| v == &'-') => {
+                    modifiers |= KeyModifiers::SHIFT;
+                    i += 1;
+                }
+                '<' if next.is_some_and(|v| v != &'>') => {} // skip, is prefix
+                '>' if next.is_none() && chars.len() > 1 => {
+                    // is suffix, end
+                    key_part_range.end = i;
+                    break;
+                }
+                _ if key_part_range.start == 0 => {
+                    key_part_range.start = i;
+                }
+                _ => {}
+            }
+            i += 1;
+        }
+
+        let key_part = &chars[key_part_range];
+        let key = match key_part.iter().collect::<String>().as_str() {
+            "BS" => KeyCode::Backspace,
+            "Backspace" => KeyCode::Backspace,
+            "CR" => KeyCode::Enter,
+            "Enter" => KeyCode::Enter,
+            "Left" => KeyCode::Left,
+            "Right" => KeyCode::Right,
+            "Up" => KeyCode::Up,
+            "Down" => KeyCode::Down,
+            "Home" => KeyCode::Home,
+            "End" => KeyCode::End,
+            "PageUp" => KeyCode::PageUp,
+            "PageDown" => KeyCode::PageDown,
+            "Tab" if modifiers.contains(KeyModifiers::SHIFT) => KeyCode::BackTab,
+            "Tab" => KeyCode::Tab,
+            "Del" => KeyCode::Delete,
+            "Insert" => KeyCode::Insert,
+            "Esc" => KeyCode::Esc,
+            "Space" => KeyCode::Char(' '),
+            "F1" => KeyCode::F(1),
+            "F2" => KeyCode::F(2),
+            "F3" => KeyCode::F(3),
+            "F4" => KeyCode::F(4),
+            "F5" => KeyCode::F(5),
+            "F6" => KeyCode::F(6),
+            "F7" => KeyCode::F(7),
+            "F8" => KeyCode::F(8),
+            "F9" => KeyCode::F(9),
+            "F10" => KeyCode::F(10),
+            "F11" => KeyCode::F(11),
+            "F12" => KeyCode::F(12),
+            c => {
+                if key_part.len() != 1 {
+                    return Err(format!("Invalid key: '{c}' from input '{s}'"));
+                }
+
+                if key_part[0].is_uppercase() {
+                    modifiers |= KeyModifiers::SHIFT;
+                }
+
+                KeyCode::Char(key_part[0])
+            }
+        };
+
+        Ok(Self { key, modifiers })
+    }
+}
+#[cfg(test)]
+#[allow(clippy::unwrap_used)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("a",            Key { key: KeyCode::Char('a'), modifiers: KeyModifiers::NONE })]
+    #[case("A",            Key { key: KeyCode::Char('A'), modifiers: KeyModifiers::SHIFT })]
+    #[case("<C-a>",        Key { key: KeyCode::Char('a'), modifiers: KeyModifiers::CONTROL })]
+    #[case("<C-A>",        Key { key: KeyCode::Char('A'), modifiers: KeyModifiers::CONTROL | KeyModifiers::SHIFT })]
+    #[case("<C-S-Tab>",    Key { key: KeyCode::BackTab,   modifiers: KeyModifiers::CONTROL | KeyModifiers::SHIFT })]
+    #[case("<S-Tab>",      Key { key: KeyCode::BackTab,   modifiers: KeyModifiers::SHIFT })]
+    #[case("<C-Tab>",      Key { key: KeyCode::Tab,       modifiers: KeyModifiers::CONTROL })]
+    #[case("<Tab>",        Key { key: KeyCode::Tab,       modifiers: KeyModifiers::NONE })]
+    #[case("5",            Key { key: KeyCode::Char('5'), modifiers: KeyModifiers::NONE })]
+    #[case("<C-A-S-5>",    Key { key: KeyCode::Char('5'), modifiers: KeyModifiers::CONTROL | KeyModifiers::SHIFT | KeyModifiers::ALT })]
+    #[case("<C-Space>",    Key { key: KeyCode::Char(' '), modifiers: KeyModifiers::CONTROL })]
+    #[case("<Space>",      Key { key: KeyCode::Char(' '), modifiers: KeyModifiers::NONE })]
+    #[case("<C-S-Space>",  Key { key: KeyCode::Char(' '), modifiers: KeyModifiers::CONTROL | KeyModifiers::SHIFT })]
+    #[case("<C-S-F11>",    Key { key: KeyCode::F(11),     modifiers: KeyModifiers::CONTROL | KeyModifiers::SHIFT })]
+    #[case("<F11>",        Key { key: KeyCode::F(11),     modifiers: KeyModifiers::NONE })]
+    #[case("<BS>",         Key { key: KeyCode::Backspace, modifiers: KeyModifiers::NONE })]
+    #[case("<C-BS>",       Key { key: KeyCode::Backspace, modifiers: KeyModifiers::CONTROL })]
+    #[case("<CR>",         Key { key: KeyCode::Enter,     modifiers: KeyModifiers::NONE })]
+    #[case("<C-CR>",       Key { key: KeyCode::Enter,     modifiers: KeyModifiers::CONTROL })]
+    #[case("<Left>",       Key { key: KeyCode::Left,      modifiers: KeyModifiers::NONE })]
+    #[case("<C-Left>",     Key { key: KeyCode::Left,      modifiers: KeyModifiers::CONTROL })]
+    #[case("<Right>",      Key { key: KeyCode::Right,     modifiers: KeyModifiers::NONE })]
+    #[case("<C-Right>",    Key { key: KeyCode::Right,     modifiers: KeyModifiers::CONTROL })]
+    #[case("<Up>",         Key { key: KeyCode::Up,        modifiers: KeyModifiers::NONE })]
+    #[case("<C-Up>",       Key { key: KeyCode::Up,        modifiers: KeyModifiers::CONTROL })]
+    #[case("<Down>",       Key { key: KeyCode::Down,      modifiers: KeyModifiers::NONE })]
+    #[case("<C-Down>",     Key { key: KeyCode::Down,      modifiers: KeyModifiers::CONTROL })]
+    #[case("<Home>",       Key { key: KeyCode::Home,      modifiers: KeyModifiers::NONE })]
+    #[case("<C-Home>",     Key { key: KeyCode::Home,      modifiers: KeyModifiers::CONTROL })]
+    #[case("<End>",        Key { key: KeyCode::End,       modifiers: KeyModifiers::NONE })]
+    #[case("<C-End>",      Key { key: KeyCode::End,       modifiers: KeyModifiers::CONTROL })]
+    #[case("<PageUp>",     Key { key: KeyCode::PageUp,    modifiers: KeyModifiers::NONE })]
+    #[case("<C-PageUp>",   Key { key: KeyCode::PageUp,    modifiers: KeyModifiers::CONTROL })]
+    #[case("<PageDown>",   Key { key: KeyCode::PageDown,  modifiers: KeyModifiers::NONE })]
+    #[case("<C-PageDown>", Key { key: KeyCode::PageDown,  modifiers: KeyModifiers::CONTROL })]
+    #[case("<Del>",        Key { key: KeyCode::Delete,    modifiers: KeyModifiers::NONE })]
+    #[case("<C-Del>",      Key { key: KeyCode::Delete,    modifiers: KeyModifiers::CONTROL })]
+    #[case("<Esc>",        Key { key: KeyCode::Esc,       modifiers: KeyModifiers::NONE })]
+    #[case("<C-Esc>",      Key { key: KeyCode::Esc,       modifiers: KeyModifiers::CONTROL })]
+    #[case(">",            Key { key: KeyCode::Char('>'), modifiers: KeyModifiers::NONE })]
+    #[case("<",            Key { key: KeyCode::Char('<'), modifiers: KeyModifiers::NONE })]
+    #[case("<C-S-<>",      Key { key: KeyCode::Char('<'), modifiers: KeyModifiers::CONTROL | KeyModifiers::SHIFT })]
+    #[case("_",            Key { key: KeyCode::Char('_'), modifiers: KeyModifiers::NONE })]
+    #[case("-",            Key { key: KeyCode::Char('-'), modifiers: KeyModifiers::NONE })]
+    #[case("<C-S-->",      Key { key: KeyCode::Char('-'), modifiers: KeyModifiers::CONTROL | KeyModifiers::SHIFT })]
+    #[case("5",            Key { key: KeyCode::Char('5'), modifiers: KeyModifiers::NONE })]
+    #[case("%",            Key { key: KeyCode::Char('%'), modifiers: KeyModifiers::NONE })]
+    fn serialization_round_trip(#[case] expected_str: &str, #[case] input: Key) {
+        let serialized = input.to_string();
+        assert_eq!(expected_str, serialized);
+
+        let deserialized: Key = serialized.parse().unwrap();
+        assert_eq!(deserialized, input);
+    }
+
+    #[rstest]
+    #[case("<Enter>",          Key { key: KeyCode::Enter,       modifiers: KeyModifiers::NONE })]
+    #[case("<Backspace>",      Key { key: KeyCode::Backspace,   modifiers: KeyModifiers::NONE })]
+    fn deserialization_extras(#[case] input: &str, #[case] expected: Key) {
+        let deserialized: Key = input.parse().unwrap();
+        assert_eq!(deserialized, expected);
+    }
+}
---
+++ src/config/keys/mod.rs
@@ -0,0 +1,233 @@
+use std::collections::HashMap;
+
+use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
+use itertools::Itertools;
+use serde::{Deserialize, Serialize};
+
+#[cfg(debug_assertions)]
+use crate::ui::screens::logs::LogsActions;
+use crate::ui::{
+    screens::{
+        albums::AlbumsActions, artists::ArtistsActions, directories::DirectoriesActions, playlists::PlaylistsActions,
+        queue::QueueActions, search::SearchActions, CommonAction,
+    },
+    GlobalAction,
+};
+
+use self::key::Key;
+
+mod key;
+
+#[derive(Debug, PartialEq, Default)]
+pub struct KeyConfig {
+    pub global: HashMap<Key, GlobalAction>,
+    pub navigation: HashMap<Key, CommonAction>,
+    pub albums: HashMap<Key, AlbumsActions>,
+    pub artists: HashMap<Key, ArtistsActions>,
+    pub directories: HashMap<Key, DirectoriesActions>,
+    pub playlists: HashMap<Key, PlaylistsActions>,
+    pub search: HashMap<Key, SearchActions>,
+    #[cfg(debug_assertions)]
+    pub logs: HashMap<Key, LogsActions>,
+    pub queue: HashMap<Key, QueueActions>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum SingleOrMultiple<T> {
+    Single(T),
+    Multiple(Vec<T>),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct KeyConfigFile {
+    #[serde(default)]
+    pub global: HashMap<GlobalAction, SingleOrMultiple<Key>>,
+    #[serde(default)]
+    pub navigation: HashMap<CommonAction, SingleOrMultiple<Key>>,
+    // pub albums: HashMap<AlbumsActions, Vec<Key>>,
+    // pub artists: HashMap<ArtistsActions, Vec<Key>>,
+    // pub directories: HashMap<DirectoriesActions, Vec<Key>>,
+    // pub playlists: HashMap<PlaylistsActions, Vec<Key>>,
+    // pub search: HashMap<SearchActions, Vec<Key>>,
+    #[cfg(debug_assertions)]
+    #[serde(default)]
+    pub logs: HashMap<LogsActions, SingleOrMultiple<Key>>,
+    #[serde(default)]
+    pub queue: HashMap<QueueActions, SingleOrMultiple<Key>>,
+}
+
+impl Default for KeyConfigFile {
+    #[rustfmt::skip]
+    #[allow(unused_imports)]
+    fn default() -> Self {
+        use GlobalAction as G;
+        use CommonAction as C;
+        use AlbumsActions as Al;
+        use ArtistsActions as Ar;
+        use DirectoriesActions as D;
+        use PlaylistsActions as P;
+        use KeyCode as K;
+        use KeyModifiers as M;
+        #[cfg(debug_assertions)]
+        use LogsActions as L;
+        use QueueActions as Q;
+        Self {
+            global: HashMap::from([
+                (G::Quit,             SingleOrMultiple::Single(Key { key: K::Char('q'), modifiers: M::NONE })),
+                (G::NextTrack,        SingleOrMultiple::Single(Key { key: K::Char('>'), modifiers: M::NONE })),
+                (G::PreviousTrack,    SingleOrMultiple::Single(Key { key: K::Char('<'), modifiers: M::NONE })),
+                (G::Stop,             SingleOrMultiple::Single(Key { key: K::Char('s'), modifiers: M::NONE })),
+                (G::ToggleRepeat,     SingleOrMultiple::Single(Key { key: K::Char('z'), modifiers: M::NONE })),
+                (G::ToggleRandom,     SingleOrMultiple::Single(Key { key: K::Char('x'), modifiers: M::NONE })),
+                (G::ToggleSingle,     SingleOrMultiple::Single(Key { key: K::Char('c'), modifiers: M::NONE })),
+                (G::TogglePause,      SingleOrMultiple::Single(Key { key: K::Char('p'), modifiers: M::NONE })),
+                (G::SeekForward,      SingleOrMultiple::Single(Key { key: K::Char('f'), modifiers: M::NONE })),
+                (G::SeekBack,         SingleOrMultiple::Single(Key { key: K::Char('b'), modifiers: M::NONE })),
+                (G::VolumeDown,       SingleOrMultiple::Single(Key { key: K::Char(','), modifiers: M::NONE })),
+                (G::VolumeUp,         SingleOrMultiple::Single(Key { key: K::Char('.'), modifiers: M::NONE })),
+                (G::PreviousTab,      SingleOrMultiple::Multiple(vec![Key { key: K::Left,      modifiers: M::NONE }, Key { key: K::BackTab,  modifiers: M::SHIFT }])),
+                (G::NextTab,          SingleOrMultiple::Multiple(vec![Key { key: K::Right,     modifiers: M::NONE }, Key { key: K::Tab,      modifiers: M::NONE }])),
+                (G::ToggleConsume,    SingleOrMultiple::Single(Key { key: K::Char('v'), modifiers: M::NONE })),
+                (G::QueueTab,         SingleOrMultiple::Single(Key { key: K::Char('1'), modifiers: M::NONE })),
+                (G::DirectoriesTab,   SingleOrMultiple::Single(Key { key: K::Char('2'), modifiers: M::NONE })),
+                (G::ArtistsTab,       SingleOrMultiple::Single(Key { key: K::Char('3'), modifiers: M::NONE })),
+                (G::AlbumsTab,        SingleOrMultiple::Single(Key { key: K::Char('4'), modifiers: M::NONE })),
+                (G::PlaylistsTab,     SingleOrMultiple::Single(Key { key: K::Char('5'), modifiers: M::NONE })),
+                (G::SearchTab,        SingleOrMultiple::Single(Key { key: K::Char('6'), modifiers: M::NONE })),
+            ]),
+            navigation: HashMap::from([
+                (C::Up,               SingleOrMultiple::Single(Key { key: K::Char('k'), modifiers: M::NONE    })),
+                (C::Down,             SingleOrMultiple::Single(Key { key: K::Char('j'), modifiers: M::NONE    })),
+                (C::MoveUp,           SingleOrMultiple::Single(Key { key: K::Char('K'), modifiers: M::SHIFT   })),
+                (C::MoveDown,         SingleOrMultiple::Single(Key { key: K::Char('J'), modifiers: M::SHIFT   })),
+                (C::Right,            SingleOrMultiple::Single(Key { key: K::Char('l'), modifiers: M::NONE    })),
+                (C::Left,             SingleOrMultiple::Single(Key { key: K::Char('h'), modifiers: M::NONE    })),
+                (C::DownHalf,         SingleOrMultiple::Single(Key { key: K::Char('d'), modifiers: M::CONTROL })),
+                (C::UpHalf,           SingleOrMultiple::Single(Key { key: K::Char('u'), modifiers: M::CONTROL })),
+                (C::Bottom,           SingleOrMultiple::Single(Key { key: K::Char('G'), modifiers: M::SHIFT   })),
+                (C::Top,              SingleOrMultiple::Single(Key { key: K::Char('g'), modifiers: M::NONE    })),
+                (C::EnterSearch,      SingleOrMultiple::Single(Key { key: K::Char('/'), modifiers: M::NONE    })),
+                (C::NextResult,       SingleOrMultiple::Single(Key { key: K::Char('n'), modifiers: M::NONE    })),
+                (C::PreviousResult,   SingleOrMultiple::Single(Key { key: K::Char('N'), modifiers: M::SHIFT   })),
+                (C::Select,           SingleOrMultiple::Single(Key { key: K::Char(' '), modifiers: M::NONE    })),
+                (C::Add,              SingleOrMultiple::Single(Key { key: K::Char('a'), modifiers: M::NONE    })),
+                (C::Delete,           SingleOrMultiple::Single(Key { key: K::Char('D'), modifiers: M::SHIFT   })),
+                (C::Rename,           SingleOrMultiple::Single(Key { key: K::Char('r'), modifiers: M::NONE    })),
+                (C::Close,            SingleOrMultiple::Multiple(vec![Key { key: K::Char('c'), modifiers: M::CONTROL }, Key { key: K::Esc, modifiers: M::NONE }])),
+                (C::Confirm,          SingleOrMultiple::Single(Key { key: K::Enter,     modifiers: M::NONE    })),
+                (C::FocusInput,       SingleOrMultiple::Single(Key { key: K::Char('i'), modifiers: M::NONE    })),
+            ]),
+            // albums: HashMap::from([
+            // ]),
+            // artists: HashMap::from([
+            // ]),
+            // directories: HashMap::from([
+            // ]),
+            // playlists: HashMap::from([
+            // ]),
+            #[cfg(debug_assertions)]
+            logs: HashMap::from([
+                (L::Clear,            SingleOrMultiple::Single(Key { key: K::Char('D'), modifiers: M::SHIFT   })),
+            ]),
+            queue: HashMap::from([
+                (Q::Delete,           SingleOrMultiple::Single(Key { key: K::Char('d'), modifiers: M::NONE    })),
+                (Q::DeleteAll,        SingleOrMultiple::Single(Key { key: K::Char('D'), modifiers: M::SHIFT   })),
+                (Q::Play,             SingleOrMultiple::Single(Key { key: K::Enter,     modifiers: M::NONE    })),
+                (Q::Save,             SingleOrMultiple::Single(Key { key: K::Char('s'), modifiers: M::CONTROL })),
+                (Q::AddToPlaylist,    SingleOrMultiple::Single(Key { key: K::Char('a'), modifiers: M::NONE    })),
+            ]),
+        }
+    }
+}
+
+fn invert_map<T: Copy, V: std::hash::Hash + std::cmp::Eq>(v: HashMap<T, SingleOrMultiple<V>>) -> HashMap<V, T> {
+    v.into_iter()
+        .flat_map(|(k, v)| match v {
+            SingleOrMultiple::Single(v) => vec![(v, k)],
+            SingleOrMultiple::Multiple(v) => v.into_iter().map(move |v| (v, k)).collect_vec(),
+        })
+        .collect()
+}
+
+impl From<KeyConfigFile> for KeyConfig {
+    fn from(value: KeyConfigFile) -> Self {
+        KeyConfig {
+            global: invert_map(value.global),
+            navigation: invert_map(value.navigation),
+            // albums: invert_map(value.albums),
+            // artists: invert_map(value.artists),
+            // directories: invert_map(value.directories),
+            // playlists: invert_map(value.playlists),
+            albums: HashMap::new(),
+            artists: HashMap::new(),
+            directories: HashMap::new(),
+            playlists: HashMap::new(),
+            search: HashMap::new(),
+            #[cfg(debug_assertions)]
+            logs: invert_map(value.logs),
+            queue: invert_map(value.queue),
+        }
+    }
+}
+
+impl From<KeyEvent> for Key {
+    fn from(value: KeyEvent) -> Self {
+        Self {
+            key: value.code,
+            modifiers: value.modifiers,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use crossterm::event::{KeyCode, KeyModifiers};
+
+    use crate::{
+        config::keys::SingleOrMultiple,
+        ui::{
+            screens::{logs::LogsActions, queue::QueueActions, CommonAction},
+            GlobalAction,
+        },
+    };
+
+    use super::{Key, KeyConfig, KeyConfigFile};
+
+    #[test]
+    #[rustfmt::skip]
+    fn converts() {
+        let input = KeyConfigFile {
+            global: HashMap::from([(GlobalAction::Quit, SingleOrMultiple::Single(Key { key: KeyCode::Char('a'), modifiers: KeyModifiers::CONTROL, }))]),
+            logs: HashMap::from([(LogsActions::Clear, SingleOrMultiple::Single(Key { key: KeyCode::Char('a'), modifiers: KeyModifiers::CONTROL, }))]),
+            queue: HashMap::from([(QueueActions::Play, SingleOrMultiple::Single(Key { key: KeyCode::Char('a'), modifiers: KeyModifiers::CONTROL, })),
+                                  (QueueActions::Save, SingleOrMultiple::Single(Key { key: KeyCode::Char('b'), modifiers: KeyModifiers::SHIFT, }))]),
+            // albums: HashMap::from([]),
+            // artists: HashMap::from([]),
+            // directories: HashMap::from([]),
+            // playlists: HashMap::from([]),
+            navigation: HashMap::from([(CommonAction::Up, SingleOrMultiple::Multiple(vec![Key { key: KeyCode::Char('a'), modifiers: KeyModifiers::CONTROL, },
+                                                               Key { key: KeyCode::Char('b'), modifiers: KeyModifiers::SHIFT }]))]),
+        };
+        let expected = KeyConfig {
+            global: HashMap::from([(Key { key: KeyCode::Char('a'), modifiers: KeyModifiers::CONTROL, }, GlobalAction::Quit)]),
+            logs: HashMap::from([(Key { key: KeyCode::Char('a'), modifiers: KeyModifiers::CONTROL, }, LogsActions::Clear)]),
+            queue: HashMap::from([(Key { key: KeyCode::Char('a'), modifiers: KeyModifiers::CONTROL, }, QueueActions::Play),
+                                  (Key { key: KeyCode::Char('b'), modifiers: KeyModifiers::SHIFT, }, QueueActions::Save)]),
+            albums: HashMap::from([]),
+            artists: HashMap::from([]),
+            directories: HashMap::from([]),
+            playlists: HashMap::from([]),
+            search: HashMap::from([]),
+            navigation: HashMap::from([(Key { key: KeyCode::Char('a'), modifiers: KeyModifiers::CONTROL }, CommonAction::Up),
+                                       (Key { key: KeyCode::Char('b'), modifiers: KeyModifiers::SHIFT }, CommonAction::Up)]),
+        };
+
+        let result: KeyConfig = input.into();
+
+
+        assert_eq!(result, expected);
+    }
+}
