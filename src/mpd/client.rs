cc69fe1d873d0ed4f18db05d0bb0794f5c9b6a60
mierak <47547062+mierak@users.noreply.github.com>
2024-06-21 23:23:05 +0200

    feat: support UNIX socket connection (#2)

--- src/mpd/client.rs
+++ src/mpd/client.rs
@@ -1,6 +1,7 @@
 use std::{
     io::{BufRead, BufReader, Write},
     net::TcpStream,
+    os::unix::net::UnixStream,
 };
 
 use crate::utils::macros::status_warn;
@@ -23,8 +24,8 @@ const MAX_SUPPORTED_VERSION: Version = Version {
 
 pub struct Client<'name> {
     name: &'name str,
-    rx: BufReader<TcpStream>,
-    stream: TcpStream,
+    rx: BufReader<TcpOrUnixStream>,
+    stream: TcpOrUnixStream,
     reconnect: bool,
     addr: &'static str,
     pub version: Version,
@@ -40,10 +41,77 @@ impl std::fmt::Debug for Client<'_> {
     }
 }
 
+enum TcpOrUnixStream {
+    Unix(UnixStream),
+    Tcp(TcpStream),
+}
+
+impl TcpOrUnixStream {
+    fn set_write_timeout(&mut self, duration: Option<std::time::Duration>) -> std::io::Result<()> {
+        match self {
+            TcpOrUnixStream::Unix(s) => {
+                s.set_write_timeout(duration)?;
+            }
+            TcpOrUnixStream::Tcp(s) => {
+                s.set_write_timeout(duration)?;
+            }
+        }
+        Ok(())
+    }
+
+    fn set_read_timeout(&mut self, duration: Option<std::time::Duration>) -> std::io::Result<()> {
+        match self {
+            TcpOrUnixStream::Unix(s) => {
+                s.set_read_timeout(duration)?;
+            }
+            TcpOrUnixStream::Tcp(s) => {
+                s.set_read_timeout(duration)?;
+            }
+        }
+        Ok(())
+    }
+
+    fn try_clone(&self) -> std::io::Result<Self> {
+        Ok(match self {
+            TcpOrUnixStream::Unix(s) => TcpOrUnixStream::Unix(s.try_clone()?),
+            TcpOrUnixStream::Tcp(s) => TcpOrUnixStream::Tcp(s.try_clone()?),
+        })
+    }
+}
+
+impl std::io::Read for TcpOrUnixStream {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        match self {
+            TcpOrUnixStream::Unix(s) => s.read(buf),
+            TcpOrUnixStream::Tcp(s) => s.read(buf),
+        }
+    }
+}
+
+impl std::io::Write for TcpOrUnixStream {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        match self {
+            TcpOrUnixStream::Unix(s) => s.write(buf),
+            TcpOrUnixStream::Tcp(s) => s.write(buf),
+        }
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        match self {
+            TcpOrUnixStream::Unix(s) => s.flush(),
+            TcpOrUnixStream::Tcp(s) => s.flush(),
+        }
+    }
+}
+
 #[allow(dead_code)]
 impl<'name> Client<'name> {
     pub fn init(addr: &'static str, name: &'name str, reconnect: bool) -> MpdResult<Client<'name>> {
-        let stream = TcpStream::connect(addr)?;
+        let mut stream = if let Some((_ip, _port)) = addr.split_once(':') {
+            TcpOrUnixStream::Tcp(TcpStream::connect(addr)?)
+        } else {
+            TcpOrUnixStream::Unix(UnixStream::connect(addr)?)
+        };
         stream.set_write_timeout(Some(std::time::Duration::from_secs(1)))?;
         stream.set_read_timeout(Some(std::time::Duration::from_secs(10)))?;
         let mut rx = BufReader::new(stream.try_clone()?);
@@ -78,7 +146,11 @@ impl<'name> Client<'name> {
     }
 
     fn reconnect(&mut self) -> MpdResult<&Client> {
-        let stream = TcpStream::connect(self.addr)?;
+        let mut stream = if let Some((_ip, _port)) = self.addr.split_once(':') {
+            TcpOrUnixStream::Tcp(TcpStream::connect(self.addr)?)
+        } else {
+            TcpOrUnixStream::Unix(UnixStream::connect(self.addr)?)
+        };
         stream.set_write_timeout(Some(std::time::Duration::from_secs(1)))?;
         stream.set_read_timeout(Some(std::time::Duration::from_secs(10)))?;
         let mut rx = BufReader::new(stream.try_clone()?);
@@ -123,7 +195,7 @@ impl<'name> SocketClient for Client<'name> {
     }
 
     fn write(&mut self, bytes: &[u8]) -> std::io::Result<()> {
-        self.stream.write_all(bytes)
+        Write::write_all(&mut self.stream, bytes)
     }
 
     fn read(&mut self) -> &mut impl BufRead {
