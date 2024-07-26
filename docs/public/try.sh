9bd528ad4918cfad2a4e16ebfee5e110f81c62b3
MieraK <mrkpwnz@gmail.com>
2024-07-26 18:36:21 +0200

    docs: fix noinstall try script

--- docs/public/try.sh
+++ docs/public/try.sh
@@ -21,15 +21,16 @@ case $(uname -s) in
 esac
 
 mkdir -p /tmp/rmpc
-curl -s -L --no-progress-meter "https://github.com/mierak/rmpc/releases/latest/download/rmpc-$arch-$system.tar.gz" | tar -C "/tmp/rmpc" -xz
+tag=$(curl -s "https://api.github.com/repos/mierak/rmpc/releases/latest" | sed -n 's/\s*"tag_name": "\(.*\)",/\1/p')
+curl -s -L --no-progress-meter "https://github.com/mierak/rmpc/releases/latest/download/rmpc-$tag-$arch-$system.tar.gz" | tar -C "/tmp/rmpc" -xz
 
 printf "Thank you for trying out rmpc! Just one more thing.\n" >&2
 printf "Rmpc needs to know the IP address and port to connect to.\n" >&2
 printf "These values can be found in your mpd config. Please provide them below.\n\n" >&2
 
-printf "Enter MPD's IP address: " >&2
+printf "Enter MPD's IP address (defaults to 127.0.0.1): " >&2
 read -r mpd_ip
-printf "Enter MPD's port: " >&2
+printf "Enter MPD's port (defaults to 6600): " >&2
 read -r mpd_port
 
-exec /tmp/rmpc/rmpc --address "$mpd_ip:$mpd_port"
+exec /tmp/rmpc/rmpc --address "${mpd_ip:-127.0.0.1}:${mpd_port:-6600}"
