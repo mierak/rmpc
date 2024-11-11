7e1067cdeb3c2ab679bad5ad62bb6c5b485c28c2
Soifou <fleur.fr@gmail.com>
2024-11-11 18:26:50 +0100

    fix: take ellipsis length into account

--- src/ui/panes/mod.rs
+++ src/ui/panes/mod.rs
@@ -693,7 +693,9 @@ impl StringExt for Cow<'_, str> {
         if self.chars().count() > max_len {
             Cow::Owned(format!(
                 "{}{}",
-                self.chars().take(max_len.saturating_sub(4)).collect::<String>(),
+                self.chars()
+                    .take(max_len.saturating_sub(symbols.ellipsis.chars().count()))
+                    .collect::<String>(),
                 symbols.ellipsis,
             ))
         } else {
@@ -707,7 +709,9 @@ impl StringExt for &str {
         if self.chars().count() > max_len {
             Cow::Owned(format!(
                 "{}{}",
-                self.chars().take(max_len.saturating_sub(4)).collect::<String>(),
+                self.chars()
+                    .take(max_len.saturating_sub(symbols.ellipsis.chars().count()))
+                    .collect::<String>(),
                 symbols.ellipsis,
             ))
         } else {
@@ -721,7 +725,9 @@ impl StringExt for String {
         if self.chars().count() > max_len {
             Cow::Owned(format!(
                 "{}{}",
-                self.chars().take(max_len.saturating_sub(4)).collect::<String>(),
+                self.chars()
+                    .take(max_len.saturating_sub(symbols.ellipsis.chars().count()))
+                    .collect::<String>(),
                 symbols.ellipsis,
             ))
         } else {
