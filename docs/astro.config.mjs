160278f9e605b73adc00d09aca42000d120a4631
mrk <mrkpwnz@gmail.com>
2024-12-20 22:53:39 +0100

    initial versioning

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import starlightLinksValidator from "starlight-links-validator";
+import starlightSidebarTopics from "starlight-sidebar-topics";
 
 import react from "@astrojs/react";
 
@@ -14,51 +15,105 @@ export default defineConfig({
     integrations: [
         starlight({
             title: "rmpc",
-            plugins: [starlightLinksValidator()],
+            plugins: [
+                starlightLinksValidator(),
+                starlightSidebarTopics([
+                    {
+                        label: "Latest git",
+                        link: "next/overview",
+                        icon: "seti:git",
+                        badge: { text: "Dev", variant: "caution" },
+                        items: [
+                            {
+                                label: "Overview",
+                                link: "next/overview",
+                            },
+                            {
+                                label: "Installation",
+                                link: "next/installation",
+                            },
+                            {
+                                label: "Try without installing",
+                                link: "next/try-without-install",
+                            },
+                            {
+                                label: "Configuration",
+                                autogenerate: {
+                                    directory: "next/configuration",
+                                },
+                            },
+                            {
+                                label: "Guides",
+                                autogenerate: {
+                                    directory: "next/guides",
+                                },
+                            },
+                            {
+                                label: "Reference",
+                                autogenerate: {
+                                    directory: "next/reference",
+                                },
+                            },
+                            {
+                                label: "Theme gallery",
+                                autogenerate: {
+                                    directory: "next/themes",
+                                },
+                            },
+                        ],
+                    },
+                    {
+                        label: "Release (v0.7.0)",
+                        link: "latest/overview",
+                        badge: { text: "Stable", variant: "default" },
+                        icon: "seti:todo",
+                        items: [
+                            {
+                                label: "Overview",
+                                link: "latest/overview",
+                            },
+                            {
+                                label: "Installation",
+                                link: "latest/installation",
+                            },
+                            {
+                                label: "Try without installing",
+                                link: "latest/try-without-install",
+                            },
+                            {
+                                label: "Configuration",
+                                autogenerate: {
+                                    directory: "latest/configuration",
+                                },
+                            },
+                            {
+                                label: "Guides",
+                                autogenerate: {
+                                    directory: "latest/guides",
+                                },
+                            },
+                            {
+                                label: "Reference",
+                                autogenerate: {
+                                    directory: "latest/reference",
+                                },
+                            },
+                            {
+                                label: "Theme gallery",
+                                autogenerate: {
+                                    directory: "latest/themes",
+                                },
+                            },
+                        ],
+                    },
+                ]),
+            ],
             editLink: {
                 baseUrl: "https://github.com/mierak/rmpc/edit/master/docs/",
             },
             social: {
                 github: "https://github.com/mierak/rmpc",
             },
-            sidebar: [
-                {
-                    label: "Overview",
-                    link: "overview",
-                },
-                {
-                    label: "Installation",
-                    link: "installation",
-                },
-                {
-                    label: "Try without installing",
-                    link: "try-without-install",
-                },
-                {
-                    label: "Configuration",
-                    autogenerate: {
-                        directory: "configuration",
-                    },
-                },
-                {
-                    label: "Guides",
-                    autogenerate: {
-                        directory: "guides",
-                    },
-                },
-                {
-                    label: "Reference",
-                    autogenerate: {
-                        directory: "reference",
-                    },
-                },
-                {
-                    label: "Theme gallery",
-                    autogenerate: {
-                        directory: "themes",
-                    },
-                },
-            ],
             customCss: ["./src/styles/custom.css"],
             components: {
                 Hero: "./src/components/Hero.astro",
