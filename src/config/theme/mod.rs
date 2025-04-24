93739abd10fcb34b1a343e8251abf90f212ff9cf
mrk <mrkpwnz@gmail.com>
2025-04-24 20:54:13 +0200

    simplify and speedup parser, support browser format, queue table

--- src/config/theme/mod.rs
+++ src/config/theme/mod.rs
@@ -1,5 +1,7 @@
 use ::serde::{Deserialize, Serialize};
 use anyhow::Result;
+use chumsky::Parser;
+use itertools::Itertools;
 use properties::{SongFormat, SongFormatFile};
 use ratatui::style::{Color, Style};
 
@@ -67,7 +69,9 @@ pub struct UiConfigFile {
     #[serde(default = "defaults::default_column_widths")]
     pub(super) browser_column_widths: Vec<u16>,
     #[serde(default)]
-    pub(super) browser_song_format: SongFormatFile,
+    // deprecated?
+    pub(super) browser_song_format: Option<SongFormatFile>,
+    pub(super) browser_song_format_v2: Option<String>,
     pub(super) background_color: Option<String>,
     pub(super) text_color: Option<String>,
     pub(super) header_background_color: Option<String>,
@@ -140,8 +144,9 @@ impl Default for UiConfigFile {
                 ellipsis: Some("...".to_owned()),
             },
             song_table_format: QueueTableColumnsFile::default(),
-            browser_song_format: SongFormatFile::default(),
             format_tag_separator: " | ".to_owned(),
+            browser_song_format: Some(SongFormatFile::default()),
+            browser_song_format_v2: None,
         }
     }
 }
@@ -240,7 +245,19 @@ impl TryFrom<UiConfigFile> for UiConfig {
                 .map_or(Ok(DEFAULT_ART as &'static [u8]), |path| -> Result<_> {
                     Ok(std::fs::read(path)?.leak())
                 })?,
-            browser_song_format: TryInto::<SongFormat>::try_into(value.browser_song_format)?,
+            browser_song_format: match (value.browser_song_format_v2, value.browser_song_format) {
+                (Some(v2), Some(_)) | (Some(v2), None) => SongFormat(
+                    parser::parser()
+                        .parse(&v2)
+                        .into_result()
+                        .map_err(|e| anyhow::anyhow!("Failed to parse property format: {:?}", e))?
+                        .into_iter()
+                        .map(|v| v.try_into())
+                        .try_collect()?,
+                ),
+                (None, Some(v1)) => TryInto::<SongFormat>::try_into(v1)?,
+                (None, None) => SongFormat::default(),
+            },
         })
     }
 }
