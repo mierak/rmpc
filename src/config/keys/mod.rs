b7d8d63ff870d88613e4c02f52daa70e1e644a5f
Othi <mnpq.raven@gmail.com>
2025-05-05 22:12:05 +0700

    correct rescan keybind

--- src/config/keys/mod.rs
+++ src/config/keys/mod.rs
@@ -5,23 +5,12 @@ pub use actions::LogsActions;
 #[cfg(debug_assertions)]
 use actions::LogsActionsFile;
 pub use actions::{
-    AlbumsActions,
-    ArtistsActions,
-    CommonAction,
-    DirectoriesActions,
-    GlobalAction,
-    PlaylistsActions,
-    QueueActions,
-    SearchActions,
+    AlbumsActions, ArtistsActions, CommonAction, DirectoriesActions, GlobalAction,
+    PlaylistsActions, QueueActions, SearchActions,
 };
 use actions::{
-    AlbumsActionsFile,
-    ArtistsActionsFile,
-    CommonActionFile,
-    DirectoriesActionsFile,
-    GlobalActionFile,
-    PlaylistsActionsFile,
-    QueueActionsFile,
+    AlbumsActionsFile, ArtistsActionsFile, CommonActionFile, DirectoriesActionsFile,
+    GlobalActionFile, PlaylistsActionsFile, QueueActionsFile,
 };
 use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
 pub use key::Key;
@@ -96,7 +85,7 @@ impl Default for KeyConfigFile {
                 (Key { key: K::Char('f'), modifiers: M::NONE  }, G::SeekForward),
                 (Key { key: K::Char('b'), modifiers: M::NONE  }, G::SeekBack),
                 (Key { key: K::Char('u'), modifiers: M::NONE  }, G::Update),
-                (Key { key: K::Char('U'), modifiers: M::NONE  }, G::Rescan),
+                (Key { key: K::Char('U'), modifiers: M::SHIFT }, G::Rescan),
                 (Key { key: K::Char(','), modifiers: M::NONE  }, G::VolumeDown),
                 (Key { key: K::Char('.'), modifiers: M::NONE  }, G::VolumeUp),
                 (Key { key: K::BackTab,   modifiers: M::SHIFT }, G::PreviousTab),
@@ -211,9 +200,7 @@ mod tests {
     #[cfg(debug_assertions)]
     use crate::config::keys::LogsActionsFile;
     use crate::config::keys::{
-        CommonAction,
-        GlobalAction,
-        QueueActions,
+        CommonAction, GlobalAction, QueueActions,
         actions::{CommonActionFile, GlobalActionFile, QueueActionsFile},
     };
 
