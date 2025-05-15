e64c9bc1937382eb121a2d5ad292f106818f92f3
mierak <47547062+mierak@users.noreply.github.com>
2025-05-16 00:10:40 +0200

    docs: add dev and contributing section (#322)

--- docs/src/content.config.ts
+++ docs/src/content.config.ts
@@ -1,7 +1,8 @@
 import { defineCollection } from "astro:content";
 import { docsSchema } from "@astrojs/starlight/schema";
 import { docsLoader } from "@astrojs/starlight/loaders";
+import { topicSchema } from "starlight-sidebar-topics/schema";
 
 export const collections = {
-    docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+    docs: defineCollection({ loader: docsLoader(), schema: docsSchema({ extend: topicSchema }) }),
 };
