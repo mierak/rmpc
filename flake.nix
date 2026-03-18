75b203e43416aaf36993d8be68b8e901af5af8d7
Matthias Ahouansou <matthias@ahouansou.cz>
2026-03-18 03:41:13 +0000

    feat(flake): switch to nightly rustfmt, allow overriding run-time dependencies and add cava as one for rmpc

--- flake.nix
+++ flake.nix
@@ -11,29 +11,26 @@
   };
 
   # NOTE: much of the following is taken from https://crane.dev/examples/quick-start-workspace.html.
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    crane,
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs = inputs: let
+    # The "scope" produced from this function is used to automatically populate arguments to
+    # functions we'll call to build packages and the devShell.
+    mkScope = pkgs:
+      pkgs.lib.makeScope pkgs.newScope (self: {
+        inherit inputs;
         lib = pkgs.lib;
-        craneLib = crane.mkLib pkgs;
+        craneLib = inputs.crane.mkLib pkgs;
         # Only includes rust/cargo files in the build source, meaning that rebuilds won't happen for unrelated files.
-        src = craneLib.cleanCargoSource ./.;
+        src = self.craneLib.cleanCargoSource ./.;
 
         # Common arguments shared across workspace dependency and package builds
         commonArgs = {
-          inherit src;
+          inherit (self) src;
           # Doesn't seem to be documented, but it was used in the crane quickstart.
           strictDeps = true;
         };
 
         # Build dependencies of the entire workspace, so that they can be re-used.
-        cargoArtifacts = craneLib.buildDepsOnly (commonArgs
+        cargoArtifacts = self.craneLib.buildDepsOnly (self.commonArgs
           // {
             # To suppress warnings when building the workspace dependencies, see
             # https://github.com/ipetkov/crane/issues/281#issuecomment-1487845029
@@ -44,13 +41,13 @@
 
         # Common attributes shared between packages
         individualCrateArgs = pname:
-          commonArgs
+          self.commonArgs
           // {
             # Use the shared built/cached workspace dependencies.
-            inherit cargoArtifacts;
+            inherit (self) cargoArtifacts;
 
             # Sets pname and version of the package from keys extracted from the Cargo.toml
-            inherit (craneLib.crateNameFromCargoToml {cargoToml = ./${pname}/Cargo.toml;}) version pname;
+            inherit (self.craneLib.crateNameFromCargoToml {cargoToml = ./${pname}/Cargo.toml;}) version pname;
 
             # Build the specified package
             cargoExtraArgs = "-p ${pname}";
@@ -58,101 +55,32 @@
 
         # Helper function to add files to the build source based on their file extension.
         workspace.root = ./.;
-        sourcesWithExt = ext: lib.fileset.fileFilter (file: file.hasExt ext) workspace.root;
+        sourcesWithExt = ext: self.lib.fileset.fileFilter (file: file.hasExt ext) self.workspace.root;
 
         # Main rust package, which can be run with `nix run github:mierak/rmpc`
         # (or just `nix run` if you're currently in this repository)
-        rmpc = craneLib.buildPackage (
-          individualCrateArgs "rmpc"
-          # This "//" syntax just merges the two sets, preferring the values on the right-side set.
-          // {
-            # A little more complicated source, as non-rust/cargo files are needed for the
-            # package/build.
-            src = lib.fileset.toSource {
-              inherit (workspace) root;
-              fileset = lib.fileset.unions [
-                # Will handle all rust/cargo related files.
-                (craneLib.fileset.commonCargoSources workspace.root)
-                # Ron files sourced for some commands.
-                (sourcesWithExt "ron")
-                # Default cover art.
-                (sourcesWithExt "jpg")
-                # Desktop file.
-                (sourcesWithExt "desktop")
-              ];
-            };
-
-            # Since the nix build is isolated, vergen won't have access to the git repository,
-            # so we'll have to set the environment variable ourselves.
-            #
-            # We can't actually (easily) get the equivalent of `git describe` unfortunately,
-            # see https://github.com/NixOS/nix/issues/7201
-            VERGEN_GIT_DESCRIBE = self.shortRev or self.dirtyShortRev;
-
-            # Additional files to be installed if the package is being, well, installed.
-            nativeBuildInputs = with pkgs; [installShellFiles];
-            postInstall = ''
-              installManPage target/man/rmpc.1
-
-              installShellCompletion --cmd rmpc \
-                --bash target/completions/rmpc.bash \
-                --fish target/completions/rmpc.fish \
-                --zsh target/completions/_rmpc
-
-              install -m 444 -D assets/rmpc.desktop $out/share/applications/rmpc.desktop
-            '';
-          }
-        );
-
+        rmpc = self.callPackage ./nix/rmpc.nix {};
         # Can be run with `nix run github:mierak/rmpc#rmpcd` (or similar to above, instead run
         # `nix run .#rmpcd` if you're currently in this repository)
-        rmpcd = craneLib.buildPackage (individualCrateArgs "rmpcd"
-          // {
-            # Like the rmpc package, we need to add extra files to the source.
-            src = lib.fileset.toSource {
-              inherit (workspace) root;
-              fileset = lib.fileset.unions [
-                # Will handle all rust/cargo related files.
-                (craneLib.fileset.commonCargoSources workspace.root)
-                # Builtin scripts
-                (sourcesWithExt "lua")
-              ];
-            };
+        rmpcd = self.callPackage ./nix/rmpcd.nix {};
 
-            # Run-time binary dependencies. We wrap the program to prepend the PATH with the
-            # required binaries.
-            nativeBuildInputs = with pkgs; [makeWrapper];
-            postInstall = ''
-              wrapProgram $out/bin/rmpcd \
-                --prefix PATH : ${with pkgs;
-                lib.makeBinPath [
-                  # Provides `notify-send` binary for builtin notify script.
-                  libnotify
-                  # Provides `xdg-open` binary for builtin lastfm script.
-                  xdg-utils
-                  # For lyrics script, and probably other things too.
-                  rmpc
-                ]}
-            '';
-          });
+        # Development shell, initialized either with `nix develop` or using direnv.
+        shell = self.callPackage ./nix/shell.nix {};
+      });
+  in
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
       in {
         packages = {
           # Add the packages defined above to the packages set, so that they can be run/built.
-          inherit rmpc rmpcd;
+          inherit (mkScope pkgs) rmpc rmpcd;
 
           # Make the default package just be rmpc.
-          default = rmpc;
+          default = (mkScope pkgs).rmpc;
         };
 
-        # Development shell, initialized either with `nix develop` or using direnv. Crane
-        # automatically sets up all the rust-specific packages & environment variables,
-        # so nothing else needs to be added.
-        devShells.default = craneLib.devShell {
-          # For some reason rust-analyzer isn't included in the default shell.
-          buildInputs = with pkgs; [
-            rust-analyzer
-          ];
-        };
+        devShells.default = (mkScope pkgs).shell;
       }
     );
 }
