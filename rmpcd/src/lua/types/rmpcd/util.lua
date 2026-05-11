dd33f2483a3202fd38d29ae25a27568b77609704
mrk <mrkpwnz@gmail.com>
2026-05-11 20:14:26 +0200

    feat: deserialize_ron for rmpcd

--- rmpcd/src/lua/types/rmpcd/util.lua
+++ rmpcd/src/lua/types/rmpcd/util.lua
@@ -5,6 +5,7 @@
 ---@field md5 fun(data: string): string
 ---@field which fun(prog: string): boolean
 ---@field nil_or_null fun(value: any): boolean
+---@field deserialize_ron fun(value: number[]): (any | nil, string | nil)
 
 ---@type Util
 ---@diagnostic disable-next-line: lowercase-global
